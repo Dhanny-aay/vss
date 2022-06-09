0b9a2bb7a9e3c4b4b35fc0c715575e4fac63da8f
Dhanny-aay <danielakintolu@gmail.com>
2022-06-09 11:23:12 +0100

    update

--- verification.js
+++ verification.js
@@ -1,3 +1,4 @@
+var result;
 $('#sendotp').one("click", function(){
     var phone_number = $("#phone_number").val();
     if (phone_number.length > 10 && phone_number != null){
@@ -5,7 +6,7 @@ $('#sendotp').one("click", function(){
         timer(60);
         $(".timer").show();
         disableResend()
-    global_var =    $.ajax({
+        $.ajax({
             type: 'POST',
             url : "https://cice-web-gateway-nodejs.herokuapp.com/v1/auth/start-phone-number-verification",
             data: JSON.stringify({
@@ -14,47 +15,54 @@ $('#sendotp').one("click", function(){
             datatype:"json",
             contentType: "application/json",
             success:function(status){
+                result = status;
                 console.log(status);
 
                 if (status = true){
-                    document.getElementById("status").innerHTML="OTP sent Successfully";
+                    document.getElementById("status").innerHTML = result.message;
                     
-                }
-                else if(status = false){
-                    document.getElementById("status").innerHTML="Please check number and try again";
                 }
             },
             error: function(){
                 document.getElementById("status").innerHTML="Please check number and try again";
                 return false;
             },
-        })
-       
-    
+        })   
     }
-    $('#sendotp').one('click',function verifyOTP(){
+});
+    $('#sendotp').one('click',function(){
       
         var phone_number = $('#phone_number').val()
-        var otp = $('#otpbox').val();
-        var verification_id = (verification_id)
+        var verification_code = $('#otpbox').val();
+        var verification_id = result.data.verification_id;
         if (otp.length == 6 && otp != null){
             $.ajax({
                 url:"http://cice-web-gateway-nodejs.herokuapp.com/v1/auth/complete-phone-number-verification",
                 type:"POST",
                 data:JSON.stringify({
                     phone_number,
-                      otp,
-                      verification_id
+                    verification_id,
+                      verification_code,
+                      
                     }),
                 datatype:"json",
                 contentType: "application/json",
-                success:function(data){
-                    console.log(data);
+
+                success:function(){
+                    console.log();
+                    document.getElementById("status").innerHTML="VErified";
+                    window.open("alertpage.html")
                     
+                },
+                error:function(){
+                    console.log()
+                    document.getElementById("status").innerHTML="Please OTP and try again";
+                    ;
                 },
             });
         };
-});});
+});
+
     // resend otp bit 
     $('#resendotp').on('click', function(){
         timer(60);
@@ -70,8 +78,9 @@ $('#sendotp').one("click", function(){
                 contentType: "application/json",
 
                 success: function(status){
+                    result = status;
                     console.log(status);
-                    document.getElementById("status").innerHTML="OTP resent";
+                    document.getElementById("status").innerHTML=result.message;
                 },
                 error: function(){
                     document.getElementById("status").innerHTML="Please check number and try again";
@@ -108,7 +117,7 @@ function timer(remaining){
 
 } 
 
-// OTP VERIFICATION
 
 
-// });
+
+
